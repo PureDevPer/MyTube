ca24951b513530749465dd3f9f7e0d6959134aaf
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-11 14:19:08 -0400

    Controllers

--- BackUp/MVC_Controller_3/routers/userRouter.js
+++ BackUp/MVC_Controller_3/routers/userRouter.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import routes from '../routers';
+import {
+	users,
+	userDetail,
+	editProfile,
+	changePassword
+} from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send('USERS'));
-userRouter.get(routes.userDetail, (req, res) => res.send('USERS_DETAILs'));
-userRouter.get(routes.editProfile, (req, res) => res.send('Edit Profile'));
-userRouter.get(routes.changePassword, (req, res) =>
-	res.send('Change Passowrd')
-);
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
--- BackUp/MVC_Controller_3/routers/videoRouter.js
+++ BackUp/MVC_Controller_3/routers/videoRouter.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import routes from '../routers';
+import {
+	vidoes,
+	upload,
+	videoDetail,
+	editVideo,
+	deleteVideo
+} from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.vidoes, (req, res) => res.send('Videos'));
-videoRouter.get(routes.upload, (req, res) => res.send('Upload'));
-videoRouter.get(routes.videoDetail, (req, res) => res.send('Videos Detail'));
-videoRouter.get(routes.editVideo, (req, res) => res.send('Edit Videos'));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send('Delete Videos'));
+videoRouter.get(routes.vidoes, vidoes);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
