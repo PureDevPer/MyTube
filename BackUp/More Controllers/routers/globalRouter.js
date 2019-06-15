62a6852e5ff7935d1320a310aa1efff72876fda5
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-15 12:59:05 -0400

    Upload, Logout, Profile, etc

---
+++ BackUp/More Controllers/routers/globalRouter.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import routes from '../routers';
+import { home, search } from '../controllers/videoController';
+import {
+	logout,
+	getJoin,
+	postJoin,
+	getLogin,
+	postLogin
+} from '../controllers/userController';
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.joins, getJoin);
+globalRouter.post(routes.joins, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.search, search);
+
+export default globalRouter;
