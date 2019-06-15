fa00fd6f4ee2950460b201134350d76c571137f6
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-15 15:43:48 -0400

    Connected MongoDB

---
+++ BackUp/Database/Connecting to MongoDB/init.js
@@ -0,0 +1,9 @@
+import './db';
+import app from './app';
+
+const PORT = 5000;
+
+const handleListening = () =>
+	console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
