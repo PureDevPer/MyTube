c0acf607a8ff0452d35aec4893e1d20628088c45
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-16 11:26:04 -0400

    Comment Model

---
+++ BackUp/Database/Comment Model/init.js
@@ -0,0 +1,14 @@
+import './db';
+import app from './app';
+import dotenv from 'dotenv';
+import './models/Video';
+import './models/Comment';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const handleListening = () =>
+	console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
