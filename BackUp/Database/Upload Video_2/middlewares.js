713950da0b1fb8eb7b72c02f45626b9a9f338a15
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-17 11:00:09 -0400

    Upload Video

---
+++ BackUp/Database/Upload Video_2/middlewares.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+import routers from './routers';
+
+const multerVideo = multer({ dest: 'uploads/videos/' });
+
+export const localsMiddleware = (req, res, next) => {
+	res.locals.siteName = 'MyTube';
+	res.locals.routes = routers;
+	res.locals.user = {
+		isAuthenticated: true,
+		id: 1
+	};
+	next();
+};
+
+export const uploadVideo = multerVideo.single('videoFile');
