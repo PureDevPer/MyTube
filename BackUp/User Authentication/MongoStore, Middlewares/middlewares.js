0a9f81a640ba9441ed87d7483a26a9ea6951a8e5
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-25 14:39:17 -0400

    MongoStore and middleware

---
+++ BackUp/User Authentication/MongoStore, Middlewares/middlewares.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import routers from "./routers";
+
+const multerVideo = multer({ dest: "uploads/videos/" });
+
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "MyTube";
+  res.locals.routes = routers;
+  res.locals.user = req.user || {};
+  // console.log(req.user);
+  next();
+};
+
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routers.home);
+  } else {
+    next();
+  }
+};
+
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routers.home);
+  }
+};
+
+export const uploadVideo = multerVideo.single("videoFile");
---
+++ BackUp/User Authentication/MongoStore, Middlewares/routers/globalRouter.js
@@ -0,0 +1,25 @@
+import express from "express";
+import routes from "../routers";
+import { home, search } from "../controllers/videoController";
+import {
+  logout,
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin
+} from "../controllers/userController";
+import { onlyPublic } from "../middlewares";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.joins, onlyPublic, getJoin);
+globalRouter.post(routes.joins, onlyPublic, postJoin, postLogin);
+
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, onlyPublic, logout);
+
+export default globalRouter;
