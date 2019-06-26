70ec107bf6f6f6e046366fe47ad7e7124e66058d
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-26 16:17:16 -0400

    User Profile

---
+++ BackUp/User Authentication/User Profile/globalRouter.js
@@ -0,0 +1,39 @@
+import express from "express";
+import passport from "passport";
+import routes from "../routers";
+import { home, search } from "../controllers/videoController";
+import {
+  logout,
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+  githubLogin,
+  postGithubLogIn,
+  getMe
+} from "../controllers/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.joins, onlyPublic, getJoin);
+globalRouter.post(routes.joins, onlyPublic, postJoin, postLogin);
+
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.gitHub, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogIn
+);
+
+globalRouter.get(routes.me, getMe);
+
+export default globalRouter;
