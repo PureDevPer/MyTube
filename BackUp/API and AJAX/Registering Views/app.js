107fd4286bce7b0ac9880fd0d9b1b2695de3c954
PureDevPer <wooseok.kim1207@gmail.com>
2019-07-03 12:53:52 -0400

    Increasing views of video

---
+++ BackUp/API and AJAX/Registering Views/app.js
@@ -0,0 +1,51 @@
+// const express = require('express');
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import apiRouter from "./routers/apiRouter";
+import router from "./routers";
+import { localsMiddleware } from "./middlewares";
+
+import "./passport";
+
+const app = express();
+
+const CookieStore = MongoStore(session);
+
+app.use(helmet());
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection })
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(localsMiddleware);
+
+app.use(router.home, globalRouter);
+app.use(router.users, userRouter);
+app.use(router.vidoes, videoRouter);
+app.use(router.api, apiRouter);
+
+export default app;
