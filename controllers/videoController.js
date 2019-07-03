93a15d3ba51ad3359fcb33e21d1b9cc0a48357c6
PureDevPer <wooseok.kim1207@gmail.com>
2019-07-03 13:44:44 -0400

    Cloning YouTube

---
+++ controllers/videoController.js
@@ -0,0 +1,159 @@
+import routes from "../routers";
+import Video from "../models/Video";
+import Comment from "../models/Comment";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({}).sort({ _id: -1 });
+    // console.log(videos);
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+};
+
+export const search = async (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  // console.log(searchingBy);
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("Search", { pageTitle: "Search", searchingBy, videos });
+};
+export const vidoes = (req, res) =>
+  res.render("videos", { pageTitle: "Videos" });
+
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path }
+  } = req;
+  // console.log(file);
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+    creator: req.user.id
+  });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
+  // console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
+};
+
+export const videoDetail = async (req, res) => {
+  // console.log(req.params.id);
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populate("comments");
+    // console.log(video);
+    res.render("videoDetail", { pageTitle: video.title, video });
+  } catch (error) {
+    // console.log(error);
+    res.redirect(routes.home);
+  }
+};
+
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    // console.log(video.title);
+    if (video.creator !== req.user.id) {
+      throw Error();
+    } else {
+      res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+    }
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
+    // console.log(title);
+    res.redirect(routes.videoDetail(id));
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    // console.log(video.title);
+    if (video.creator !== req.user.id) {
+      throw Error();
+    } else {
+      // https://mongoosejs.com/docs/api.html#model_Model.findOneAndRemove
+      await Video.findOneAndRemove({ _id: id });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect(routes.home);
+};
+
+// Register Video View
+
+export const postRegisterView = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    video.views += 1;
+    video.save();
+    res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+// Add Comment
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id
+    });
+    video.comments.push(newComment.id);
+    video.save();
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
